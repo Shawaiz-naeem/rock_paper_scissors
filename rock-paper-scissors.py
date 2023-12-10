10eaad6dff91a97fcd97b89879272ccdbcd74e97
shawaiz-naeem <shawaizn3@gmail.com>
2023-12-10 09:34:49 +0000

    Player go and computer go

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,11 +1,22 @@
 import random
 decider = random.randint(1,3)
-go = None
+computer_go = None
 if decider == 1:
-    go = 'rock'
+    computer_go = 'rock'
 elif decider == 2:
-    go = 'paper'
+    computer_go = 'paper'
 elif decider == 3:
-    go = 'scissors'
-print(go)
+    computer_go = 'scissors'
+
+
+
+while True:
+    player_go = input("Pick 'rock', paper' or 'scissors'")
+    if player_go == 'rock' or player_go == 'paper' or player_go == 'scissors':
+        break
+    else:
+        print('pick an option')
+print("The computer picked {}".format(computer_go))
+print("You picked {}".format(player_go))
+
 
